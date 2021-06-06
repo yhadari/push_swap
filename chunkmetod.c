7c1afc8f13d7f39ce19edf8ed22989c939df1ee1
Yacine Hadari <yhadari@e1r3p10.1337.ma>
2021-06-06 18:25:50 +0100

    finish

---
+++ chunkmetod.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chunkmetod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhadari <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/02 14:41:05 by yhadari           #+#    #+#             */
+/*   Updated: 2021/06/06 15:01:52 by yhadari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+void	push_max_toa2(t_stack *a, t_stack *b, int *size, int max)
+{
+	if (max == b->data[b->top])
+	{
+		pa(a, b, 1);
+		*size = b->top;
+	}
+	else if (max == b->data[*size])
+	{
+		if (*size > b->top / 2)
+			find_rotate(*size, a, b, 2);
+		else
+		{
+			while ((*size)-- >= 0)
+				rrb(b, 1);
+			pa(a, b, 1);
+		}
+		*size = b->top;
+	}
+	else
+		(*size)--;
+}
+
+void	push_max_toa(t_stack *a, t_stack *b)
+{
+	t_sort	tab;
+	int		max;
+	int		size;
+
+	tab = sort(*b);
+	max = tab.str[b->top];
+	ft_free(&tab);
+	size = b->top;
+	while (b->top >= 0)
+	{
+		tab = sort(*b);
+		max = tab.str[b->top];
+		ft_free(&tab);
+		push_max_toa2(a, b, &size, max);
+	}
+}
+
+void	rotate(t_stack *a, t_stack *b, t_chunk *chunk)
+{
+	find_rotate(chunk->size_a, a, b, 1);
+	chunk->index++;
+}
+
+void	push_chunk_tob(t_stack *a, t_stack *b, t_chunk *chunk, t_map *map)
+{
+	chunk->size_a = a->top;
+	chunk->index = 0;
+	while (chunk->size_a >= 0 && chunk->index < chunk->size)
+	{
+		if (isinchunk(map, a->data[chunk->size_a], chunk->nb, chunk->size))
+		{
+			if (chunk->size_a > a->top / 2)
+				rotate(a, b, chunk);
+			else
+			{
+				while (chunk->size_a-- >= 0)
+				{
+					rra(a, 1);
+					if (isinchunk(map, a->data[a->top], chunk->nb, chunk->size))
+					{
+						pb(a, b, 1);
+						chunk->index++;
+					}
+				}
+			}
+			chunk->size_a = a->top;
+		}
+		else
+			chunk->size_a--;
+	}
+}
+
+void	chunkmetod(t_sort buf, t_stack *a, t_stack *b)
+{
+	t_chunk	chunk;
+	t_map	*map;
+	int		size;
+
+	chunk.nb = 0;
+	size = 0;
+	chunk.size = (a->top + 1) / 10;
+	map = malloc(sizeof(t_map) * a->top + 1);
+	while (size <= a->top)
+	{
+		map[size].nb = buf.str[size];
+		map[size++].chunk = chunk.nb;
+		if (size % chunk.size == 0)
+			chunk.nb++;
+	}
+	chunk.nb = 0;
+	while (a->top >= 0)
+	{
+		push_chunk_tob(a, b, &chunk, map);
+		chunk.nb++;
+	}
+	free(map);
+}
---
+++ lib.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhadari <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/03 21:13:43 by yhadari           #+#    #+#             */
+/*   Updated: 2021/06/06 18:12:38 by yhadari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str++)
+		i++;
+	return (i);
+}
+
+int	ft_isdigit(char *str)
+{
+	while (*str)
+	{
+		if ((*str <= 47 || *str >= 58) && *str != '-' && *str != '+')
+			return (0);
+		if ((*str == '-' || *str == '+')
+			&& (*(str + 1) > 57 || *(str + 1) < 48))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+void	checkint(char *str, int i)
+{
+	if (ft_strlen(str) > 11)
+		ft_perror("Error\n");
+	if (ft_strlen(str) > 10 && str[i] != '-' && str[i] != '+')
+		ft_perror("Error\n");
+}
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	result = 0;
+	checkint(str, i);
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			j = -1;
+		i++;
+	}
+	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if ((result < 0 && j == -1 && result != -2147483648)
+		|| (result < 0 && j == 1))
+		ft_perror("Error\n");
+	return (result * j);
+}
+
+int	ft_perror(char *str)
+{
+	write(1, str, ft_strlen(str) + 1);
+	exit(0);
+	return (0);
+}
